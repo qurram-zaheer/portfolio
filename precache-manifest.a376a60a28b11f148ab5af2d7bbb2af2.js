28aff792fbfa01c0144cbb30772b9d22c6503e98
Qurram Zaheer <qurramzaheer@Qurrams-MacBook-Air.local>
2020-01-23 19:23:59 +0530

    Updates

---
+++ precache-manifest.a376a60a28b11f148ab5af2d7bbb2af2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a62464c170caff13aa1f6fb6389aded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2441e7eaa46296291849",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "883afd60abcff91b80f6",
+    "url": "/static/css/main.d030d801.chunk.css"
+  },
+  {
+    "revision": "2441e7eaa46296291849",
+    "url": "/static/js/2.ae1e607d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ae1e607d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "883afd60abcff91b80f6",
+    "url": "/static/js/main.421df464.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "2ed1b46f10d4cd89e67b4facb3a23c3f",
+    "url": "/static/media/bgp.2ed1b46f.png"
+  },
+  {
+    "revision": "9e7c9996e62919b6a1e6c1821fd7fbf4",
+    "url": "/static/media/dp.9e7c9996.jpeg"
+  },
+  {
+    "revision": "0a1d84548bc106d41a1fffa97caedc87",
+    "url": "/static/media/git.0a1d8454.png"
+  },
+  {
+    "revision": "7ce98345905507166ea893ea002abfff",
+    "url": "/static/media/jsr.7ce98345.png"
+  },
+  {
+    "revision": "c30da267e561a3fd535db64d11277be5",
+    "url": "/static/media/qdex.c30da267.png"
+  },
+  {
+    "revision": "8a09d3424ae70831eb418ccdfb5b6b45",
+    "url": "/static/media/qf.8a09d342.png"
+  }
+]);
